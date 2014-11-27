e99dd12a89e350f0e4fbc45ebcb93ce1fb9d3808
JamesLeo <vivlonglz@gmail.com>
2014-11-27 15:33:24 +0800

    Add Database

--- TesseractForNetDemo/Main.cs
+++ TesseractForNetDemo/Main.cs
@@ -6,14 +6,59 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Tesseract;
 
 namespace TesseractForNetDemo
 {
   public partial class Main : Form
   {
+    private string strSelectImgFile = "";
+
     public Main()
     {
       InitializeComponent();
+    }
+
+    private void btn_1_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog openFileDialog1 = new OpenFileDialog();
+      openFileDialog1.InitialDirectory = Application.StartupPath;
+      openFileDialog1.Filter = "TIF files(*.tif)|*.tif";
+      openFileDialog1.RestoreDirectory = true;
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        strSelectImgFile = openFileDialog1.FileName;
+        pictureBox1.Image = Image.FromFile(strSelectImgFile);
+      }
+    }
+
+    private void btn_2_Click(object sender, EventArgs e)
+    {
+      if (strSelectImgFile.Length < 1) return;
+      try
+      {
+        using (var engine = new TesseractEngine(@"./tessdata", "chi_sim", EngineMode.Default))
+        {
+          using (var img = Pix.LoadFromFile(strSelectImgFile))
+          {
+            using (var page = engine.Process(img))
+            {
+              var text = page.GetText();
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+
+    private void Main_Load(object sender, EventArgs e)
+    {
+      // TODO: 这行代码将数据加载到表“db_Simple_Imgr2DataSet.Imgr2”中。您可以根据需要移动或删除它。
+      this.imgr2TableAdapter.Fill(this.db_Simple_Imgr2DataSet.Imgr2);
+
     }
   }
 }
