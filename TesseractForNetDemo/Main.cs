eb6adc7fdeb13aa0a35c5a6c377d31861e160dc1
wuilovelizhen <wuilovelizhen@163.com>
2014-12-03 14:14:27 +0800

    加鼠标滚动该图大小

--- TesseractForNetDemo/Main.cs
+++ TesseractForNetDemo/Main.cs
@@ -29,6 +29,7 @@ namespace TesseractForNetDemo
       {
         strSelectImgFile = openFileDialog1.FileName;
         pictureBox1.Image = Image.FromFile(strSelectImgFile);
+        pictureBox1.Size = pictureBox1.Image.Size;
       }
     }
 
@@ -58,8 +59,25 @@ namespace TesseractForNetDemo
     private void Main_Load(object sender, EventArgs e)
     {
       // TODO: 这行代码将数据加载到表“db_Simple_Imgr2DataSet.Imgr2”中。您可以根据需要移动或删除它。
+        this.pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+        this.pictureBox1.MouseEnter += new EventHandler(pictureBox1_MouseEnter);
       this.imgr2TableAdapter.Fill(this.db_Simple_Imgr2DataSet.Imgr2);
 
+    }
+    private void pictureBox1_MouseEnter(object sender, EventArgs e)
+    {
+        this.pictureBox1.Focus();
+    }
+
+    private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+    {
+        double dbScale = 1;
+        if (this.pictureBox1.Height > 0)
+        {
+            dbScale = (double)this.pictureBox1.Width / (double)this.pictureBox1.Height;
+        }
+        pictureBox1.Width += (int)(e.Delta * dbScale);
+        pictureBox1.Height += e.Delta;
     }
   }
 }
